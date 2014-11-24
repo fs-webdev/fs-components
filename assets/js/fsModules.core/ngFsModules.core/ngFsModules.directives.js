8e523f74343f672ae96c0966640b4bbfce9899ee
Steven Lambert <steven.lambert@ldschurch.org>
2014-11-24 11:50:23 -0700

    fix(fsModules) - fsAddWrapperIf directive correctly removes elements on false value
    fixes #15

--- assets/js/fsModules.core/ngFsModules.core/ngFsModules.directives.js
+++ assets/js/fsModules.core/ngFsModules.core/ngFsModules.directives.js
@@ -6,11 +6,11 @@
  */
 
 /**
- * Conditionally add the wrapper element if the expression results to true, otherwise replace the element with it's children.
- * Functionality modeled after ngIf from Angularjs 1.2.0-rc.2 since that is the version of Angularjs we are using in tree.
- * @see {@link https://github.com/angular/angular.js/blob/v1.2.0-rc.2/src/ng/directive/ngIf.js|ngIf}
+ * Conditionally add the wrapper element if the expression results to true, otherwise replace the
+ * element with it's children.
+ * @see {@link https://github.com/angular/angular.js/blob/v1.2.16/src/ng/directive/ngIf.js|ngIf}
  *
- * @since 1.0.2
+ * @since 1.0.3
  */
 angular.module('ngFsModules')
        .directive('fsAddWrapperIf', ['$animate', function($animate) {
@@ -18,39 +18,41 @@ angular.module('ngFsModules')
     transclude: 'element',
     priority: 1000,
     restrict: 'A',
-    compile: function (element, attr, transclude) {
-      return function ($scope, $element, $attr) {
-        var childElement, childScope;
-        $scope.$watch($attr.fsAddWrapperIf, function fsAddWrapperIfWatchAction(value) {
-          if (childElement) {
-            $animate.leave(childElement);
-            childElement = undefined;
-          }
-          if (childScope) {
-            childScope.$destroy();
-            childScope = undefined;
-          }
+    $$tlb: true,
+    link: function ($scope, $element, $attr, ctrl, $transclude) {
+      var childElement, childScope;
+      $scope.$watch($attr.fsAddWrapperIf, function fsAddWrapperIfWatchAction(value) {
+        if (childElement) {
+          $animate.leave(childElement);
+          childElement = undefined;
+        }
+        if (childScope) {
+          childScope.$destroy();
+          childScope = undefined;
+        }
 
-          // add the wrapper
-          if (value) {
-            childScope = $scope.$new();
-            transclude(childScope, function (clone) {
-              childElement = clone;
-              $animate.enter(clone, $element.parent(), $element);
+        childScope = $scope.$new();
+
+        // add the wrapper
+        if (value) {
+          $transclude(childScope, function (clone) {
+            childElement = clone;
+            $animate.enter(clone, $element.parent(), $element);
+          });
+        }
+        // remove the wrapper
+        else {
+          $transclude(childScope, function (clone) {
+            // https://github.com/fs-webdev/fs-modules/issues/15
+            // save the reference to the children so that $animate.leave removes them and not the
+            // parent element (which becomes detached from the children after replaceWith)
+            childElement = clone.children();
+            $animate.enter(clone, $element.parent(), $element, function() {
+              clone.replaceWith(clone.children());
             });
-          }
-          // remove the wrapper
-          else {
-            childScope = $scope.$new();
-            transclude(childScope, function (clone) {
-              $animate.enter(clone, $element.parent(), $element, function() {
-                childElement = clone.contents();
-                clone.replaceWith(clone.contents());
-              });
-            });
-          }
-        });
-      };
+          });
+        }
+      });
     }
   };
 }]);

7b26c3be4417b79e4cca08afc9916943d952327b
Steven Lambert <steven.lambert@ldschurch.org>
2014-12-02 15:10:37 -0700

    feature(fsPerson) - Show parents option added. Will put the parents as a couple relationship inside fsPersonVitals and allow you to hide and show them.

--- assets/js/fsPerson/fsPersonVitals.directive.js
+++ assets/js/fsPerson/fsPersonVitals.directive.js
@@ -1,3 +1,5 @@
+var uid = 0;  // unique id needed for show/hide parent label + input
+
 /**
  * Output a person's vitals (name, lifeSpan, and id).
  * @param {object} person - Person to render.
@@ -14,14 +16,25 @@
  *
  * @returns {element}
  *
- * @since 1.1.0
+ * @since 1.3.0
  */
 fsModules.registerDirective('fsPersonVitals', ['person', 'options', function(person, options) {
   var scope = fsPersonVitalsViewModel({person: person, options: options});
 
+  scope.uid = uid++;
+
   // convert the angular template
-  var templateStr = templateList.fsPersonVitals;
+  var templateStr = getTemplateStr('fsPersonVitals');
   var template = this.parseTemplate(templateStr, scope);
+
+  // add parents in the link function since Angular will get into
+  // an infinite state if it is added to the DOM (even behind and ng-if).
+  // This implies that Angular tries to parse the entire DOM tree before it
+  // does any ng calculations.
+  if (scope.options.showParents && scope.person.parents) {
+    var parents = this.fsCoupleInfo(scope.person.parents.father, scope.person.parents.mother, {showParents: false, iconSize: 'small'});
+    template.querySelector('.fs-person__fs-person-parents').appendChild(parents);
+  }
 
   return template;
 }], {restrict: 'E', replace: true});
--- assets/js/fsPerson/ngFsPerson/ngFsPersonVitals.directive.js
+++ assets/js/fsPerson/ngFsPerson/ngFsPersonVitals.directive.js
@@ -1,14 +1,16 @@
 /**
  * Render a person's vitals (name, lifeSpan, and id).
  *
- * @since 1.1.0
+ * @since 1.3.0
  */
+var uid = 0;  // unique id needed for show/hide parent label + input
+
 angular.module('ngFsModules')
-       .directive('fsPersonVitals', function() {
+       .directive('fsPersonVitals', ['$compile', function($compile) {
   return {
     restrict: 'E',
     replace: true,
-    template: templateList.fsPersonVitals,
+    template: getTemplateStr('fsPersonVitals'),
     scope: {
       person: '=',
       config: '&',
@@ -16,11 +18,22 @@ angular.module('ngFsModules')
     link: function(scope, element, attrs) {
       scope.options = scope.config() || {};
 
+      scope.uid = uid++;
+
       scope = fsPersonVitalsViewModel(scope);
 
       scope.$watch('person', function fsPersonVitalsWatchAction() {
         scope = fsPersonVitalsViewModel(scope);
+
+        // add parents in the link function since Angular will get into
+        // an infinite state if it is added to the DOM (even behind and ng-if).
+        // This implies that Angular tries to parse the entire DOM tree before it
+        // does any ng calculations.
+        if (scope.options.showParents && scope.person.parents) {
+          var parents = $compile('<fs-couple-info data-husband="person.parents.father" data-wife="person.parents.mother" data-config="{showParents: false, iconSize: \'small\'}"></fs-couple-info>')(scope);
+          element.find('.fs-person__fs-person-parents').append(parents);
+        }
       });
     }
   };
-});
+}]);
